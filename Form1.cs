3a54ee3c32896bec42504305c20d7fd5c87bfc6e
ertugrululuhan <111891237+ertugrululuhan@users.noreply.github.com>
2023-02-27 19:19:09 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barkodcu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+            private void btnbarcode_Click(object sender, EventArgs e)
+            {
+                Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+                pictureBox1.Image = barcode.Draw(textBox1.Text, 50);
+        }
+            private void btnqrcode_Click(object sender, EventArgs e)
+            {
+                Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+                pictureBox1.Image = qrcode.Draw(textBox2.Text, 50);
+            }
+
+        
+    }
+}
